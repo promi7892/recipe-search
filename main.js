f2c62b704d79375b0364b5163fecce0554a85e44
Promi Jarin Tasnim <orick@Ks-MacBook-Pro.local>
2021-08-28 04:22:55 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const searchFood = () => {
+	const searchField = document.getElementById('search-field');
+	const searchText = searchField.value;
+	searchField.value = '';
+	if (searchText == '') {
+		alert('Please write the recipe name');
+	}
+	// clear data
+	else {
+		const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+		fetch(url)
+			.then((resonse) => resonse.json())
+			.then((data) => displaySearchResult(data.meals));
+	}
+};
+
+const displaySearchResult = (meals) => {
+	const searchResult = document.getElementById('search-result');
+	searchResult.textContent = '';
+	// if (meals.length === 0) {
+	// 	alert('not found');
+	// }
+	meals.forEach((meal) => {
+		const div = document.createElement('div');
+		div.classList.add('col');
+		div.innerHTML = `
+        <div onclick = "loadMealDetail(${meal.idMeal})"
+        class="card h-100 bg-dark">
+					<img src="${meal.strMealThumb}" class="card-img-top" />
+					<div class="card-body ">
+						<h5 class="card-title text-black">${meal.strMeal}</h5>
+						<p class="card-text">
+							${meal.strInstructions.slice(0, 250)}
+						</p>
+					</div>
+				</div>
+        `;
+		searchResult.append(div);
+	});
+};
+
+const loadMealDetail = (mealId) => {
+	const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => displayMealDetail(data.meals[0]));
+};
+
+const displayMealDetail = (meal) => {
+	console.log(meal);
+	const mealDetails = document.getElementById('meal-details');
+	const div = document.createElement('div');
+	div.classList.add('card');
+	div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body bg-dark py-3">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text ">${meal.strInstructions.slice(0, 150)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-secondary">View Recipe</a>
+    </div>
+    `;
+	mealDetails.appendChild(div);
+};
